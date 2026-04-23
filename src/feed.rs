a1affc2f729cf2b79e4fa173cd4369d586e54aa1
n0g.rip <gpg@n0g.rip>
2026-04-23 17:40:26 +0100

    format

--- src/feed.rs
+++ src/feed.rs
@@ -3,82 +3,91 @@ use rss::*;
 
 #[derive(Clone, Hash, PartialEq, Eq)]
 pub struct Channel {
-	pub atom: String,
-	icon: String
+    pub atom: String,
+    icon: String,
 }
 
 impl Channel {
-	// Parses the homepage to extract url to the Atom feed and the channel's icon
-	pub fn new(homepage: String) -> Channel {
-		Channel {
-			atom: Regex::new(r#"<link rel="alternate" type="application/rss\+xml" title="RSS" href="(.*?)">"#)
-				.expect("Regex is valid")
-				.captures(&homepage).unwrap()
-				.get(1).expect("Regex contains a capture")
-				.as_str().to_string(),
-			icon: Regex::new(r#"<meta property="og:image" content="(.*?)">"#)
-				.expect("Regex is valid")
-				.captures(&homepage).unwrap()
-				.get(1).expect("Regex contains a capture")
-				.as_str().to_string()
-				.replace("=s900", "=s128") // Request smaller icon
-		}
-	}
+    // Parses the homepage to extract url to the Atom feed and the channel's icon
+    pub fn new(homepage: String) -> Channel {
+        Channel {
+            atom: Regex::new(
+                r#"<link rel="alternate" type="application/rss\+xml" title="RSS" href="(.*?)">"#,
+            )
+            .expect("Regex is valid")
+            .captures(&homepage)
+            .unwrap()
+            .get(1)
+            .expect("Regex contains a capture")
+            .as_str()
+            .to_string(),
+            icon: Regex::new(r#"<meta property="og:image" content="(.*?)">"#)
+                .expect("Regex is valid")
+                .captures(&homepage)
+                .unwrap()
+                .get(1)
+                .expect("Regex contains a capture")
+                .as_str()
+                .to_string()
+                .replace("=s900", "=s128"), // Request smaller icon
+        }
+    }
 }
 
 pub fn rss_feed(channel: Channel, atom: feed_rs::model::Feed, host: &str) -> String {
-	ChannelBuilder::default()
-		.title(atom.title.unwrap().content)
-		.image(
-			ImageBuilder::default()
-				.url(channel.icon)
-				.build()
-		)
-		.items(
-			atom.entries
-				.into_iter()
-				.filter_map(|e| {
-					let id = e.id.split(":").last()?.to_string();
-					Some(
-						ItemBuilder::default()
-							.guid( Guid { value: id.clone(), permalink: false } )
-							.link(e.links.first()?.clone().href)
-							.title(e.title?.content)
-							.content(html_description(&e.media.first()?.clone().description?.content))
-							.pub_date(e.published?.to_rfc2822())
-							.enclosure(
-								EnclosureBuilder::default()
-									.url(format!("{}/{}.mp4", host, id))
-									.mime_type("video/mp4")
-									.build()
-							)
-							.build()
-					)
-				})
-				.collect::<Vec<rss::Item>>()
-		)
-		.build()
-		.to_string()
+    ChannelBuilder::default()
+        .title(atom.title.unwrap().content)
+        .image(ImageBuilder::default().url(channel.icon).build())
+        .items(
+            atom.entries
+                .into_iter()
+                .filter_map(|e| {
+                    let id = e.id.split(":").last()?.to_string();
+                    Some(
+                        ItemBuilder::default()
+                            .guid(Guid {
+                                value: id.clone(),
+                                permalink: false,
+                            })
+                            .link(e.links.first()?.clone().href)
+                            .title(e.title?.content)
+                            .content(html_description(
+                                &e.media.first()?.clone().description?.content,
+                            ))
+                            .pub_date(e.published?.to_rfc2822())
+                            .enclosure(
+                                EnclosureBuilder::default()
+                                    .url(format!("{}/{}.mp4", host, id))
+                                    .mime_type("video/mp4")
+                                    .build(),
+                            )
+                            .build(),
+                    )
+                })
+                .collect::<Vec<rss::Item>>(),
+        )
+        .build()
+        .to_string()
 }
 
 // Makes links clickable and formats line breaks
 fn html_description(string: &str) -> String {
-	let link_or_line_break = Regex::new(r#"(https?://[^\s]+|\n)"#).expect("Regex is valid");
-	let mut match_end: usize = 0;
-	let mut html = String::new();
-	link_or_line_break
-		.captures_iter(string)
-		.map(|c| c.get(0).unwrap())
-		.for_each(|regex_match| {
-			let match_string = regex_match.as_str();
-			let replacement = if match_string == "\n" {
-				"<br>".to_string()
-			} else {
-				format!(r#"<a href="{match_string}">{match_string}</a>"#)
-			};
-			html.push_str(&string[match_end..regex_match.start()]);
-			html.push_str(&replacement);
-			match_end = regex_match.end();
-		});
-	html
+    let link_or_line_break = Regex::new(r#"(https?://[^\s]+|\n)"#).expect("Regex is valid");
+    let mut match_end: usize = 0;
+    let mut html = String::new();
+    link_or_line_break
+        .captures_iter(string)
+        .map(|c| c.get(0).unwrap())
+        .for_each(|regex_match| {
+            let match_string = regex_match.as_str();
+            let replacement = if match_string == "\n" {
+                "<br>".to_string()
+            } else {
+                format!(r#"<a href="{match_string}">{match_string}</a>"#)
+            };
+            html.push_str(&string[match_end..regex_match.start()]);
+            html.push_str(&replacement);
+            match_end = regex_match.end();
+        });
+    html
 }
--- src/main.rs
+++ src/main.rs
@@ -2,131 +2,139 @@ use actix_web::{http::header::*, web::*, *};
 use cached::proc_macro::cached;
 use clap::*;
 use feed_rs::parser;
-use std::{
-	collections::hash_map::DefaultHasher, 
-	hash::Hasher, 
-	process::Command
-};
+use std::{collections::hash_map::DefaultHasher, hash::Hasher, process::Command};
 mod feed;
 use feed::*;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-	/// Bind address
-	#[arg(long, default_value = "localhost")]
-	bind: String,
+    /// Bind address
+    #[arg(long, default_value = "localhost")]
+    bind: String,
 
-	/// Port to listen on
-	#[arg(long, default_value_t = 8080)]
-	port: u16,
-	
-	/// Optional Public URL, if service is hosted behind a reverse proxy
-	#[arg(long, default_value = None)]
-	host: Option<String>
+    /// Port to listen on
+    #[arg(long, default_value_t = 8080)]
+    port: u16,
+
+    /// Optional Public URL, if service is hosted behind a reverse proxy
+    #[arg(long, default_value = None)]
+    host: Option<String>,
 }
 
 impl Args {
-	fn host(&self) -> String {
-		self.host.clone()
-			.unwrap_or(format!("http://{}:{}", self.bind, self.port))
-	}
+    fn host(&self) -> String {
+        self.host
+            .clone()
+            .unwrap_or(format!("http://{}:{}", self.bind, self.port))
+    }
 }
 
 #[derive(Clone)]
-struct AppState { host: String }
+struct AppState {
+    host: String,
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-	let args = Args::parse();
-	let data = AppState { host: args.host() };
-	HttpServer::new(move || {
-		App::new()
-			.app_data(Data::new(data.clone()))
-			.service(rss)
-			.service(src)
-	})
-	.bind((args.bind, args.port))?
-	.run()
-	.await
+    let args = Args::parse();
+    let data = AppState { host: args.host() };
+    HttpServer::new(move || {
+        App::new()
+            .app_data(Data::new(data.clone()))
+            .service(rss)
+            .service(src)
+    })
+    .bind((args.bind, args.port))?
+    .run()
+    .await
 }
 
 // Returns RSS feed for a channel
 // Regex: youtube channel's unique handle
 #[get("/{handle:@[A-Za-z0-9-_.]{3,30}}")]
-async fn rss(handle: Path<String>, request: HttpRequest, app_state: Data<AppState>) -> HttpResponse {
-	// Fetch sources
-	let channel = channel(handle.into_inner()).await;
-	let atom_bytes = atom_bytes(channel.atom.clone()).await;
-	
-	// Calculate hash
-	let mut hasher = DefaultHasher::new();
-	hasher.write(&atom_bytes);
-	let hash = format!("{:x}", hasher.finish());
-	
-	// Check if ETag matches and return 304 (not modified) if it does
-	if let Some(request_etag) = request.headers().get(http::header::IF_NONE_MATCH) {
-		if request_etag.to_str().expect("ETag is ASCII Encoded") == format!("{}", EntityTag::new_strong(hash.clone())) {
-			return HttpResponse::NotModified().finish()
-		}
-	}
-	
-	// If not create and return feed
-	HttpResponse::Ok()
-		.content_type(ContentType::json())
-		.insert_header(ETag(EntityTag::new_strong(hash)))
-		.body(
-			rss_feed(
-				channel.clone(), 
-				parser::parse(atom_bytes.as_ref()).expect("YT Atom is valid"),
-				&app_state.host
-			)
-		)
+async fn rss(
+    handle: Path<String>,
+    request: HttpRequest,
+    app_state: Data<AppState>,
+) -> HttpResponse {
+    // Fetch sources
+    let channel = channel(handle.into_inner()).await;
+    let atom_bytes = atom_bytes(channel.atom.clone()).await;
+
+    // Calculate hash
+    let mut hasher = DefaultHasher::new();
+    hasher.write(&atom_bytes);
+    let hash = format!("{:x}", hasher.finish());
+
+    // Check if ETag matches and return 304 (not modified) if it does
+    if let Some(request_etag) = request.headers().get(http::header::IF_NONE_MATCH) {
+        if request_etag.to_str().expect("ETag is ASCII Encoded")
+            == format!("{}", EntityTag::new_strong(hash.clone()))
+        {
+            return HttpResponse::NotModified().finish();
+        }
+    }
+
+    // If not create and return feed
+    HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .insert_header(ETag(EntityTag::new_strong(hash)))
+        .body(rss_feed(
+            channel.clone(),
+            parser::parse(atom_bytes.as_ref()).expect("YT Atom is valid"),
+            &app_state.host,
+        ))
 }
 
 // Returns video source
 // Regex: youtube video's unique id
 #[get("/{id:[A-Za-z0-9-_.]{11}}.mp4")]
 async fn src(id: Path<String>) -> HttpResponse {
-	HttpResponse::TemporaryRedirect()
-		.append_header((
-			"location", 
-			source(id.into_inner())
-		))
-		.finish()
+    HttpResponse::TemporaryRedirect()
+        .append_header(("location", source(id.into_inner())))
+        .finish()
 }
 
 // Resolved handles are cached in memory
 #[cached(sync_writes = true)]
 async fn channel(handle: String) -> Channel {
-	Channel::new(
-		reqwest::Client::new()
-			.get(format!("https://www.youtube.com/{handle}"))
-			.send().await.unwrap()
-			.text().await.unwrap()
-	)
+    Channel::new(
+        reqwest::Client::new()
+            .get(format!("https://www.youtube.com/{handle}"))
+            .send()
+            .await
+            .unwrap()
+            .text()
+            .await
+            .unwrap(),
+    )
 }
 
 // Atom sources should be valid for 15 minutes
-#[cached(time=900, sync_writes = true)]
+#[cached(time = 900, sync_writes = true)]
 async fn atom_bytes(atom: String) -> actix_web::web::Bytes {
-	reqwest::get(atom).await.unwrap()
-		.bytes().await.unwrap()
+    reqwest::get(atom).await.unwrap().bytes().await.unwrap()
 }
 
 // Video sources should be valid for 6 hours
-#[cached(time=20_000, sync_writes = true)]
+#[cached(time = 20_000, sync_writes = true)]
 fn source(id: String) -> String {
-	String::from_utf8(
-		Command::new("yt-dlp")
-		.args([
-			"--get-url",
-			"--force-ipv4",
-			"--no-warnings",
-			"-f", "18",
-			&format!("https://youtu.be/{id}")
-		]).output().unwrap().stdout
-	).unwrap()
-	.trim_end()
-	.to_string()
+    String::from_utf8(
+        Command::new("yt-dlp")
+            .args([
+                "--get-url",
+                "--force-ipv4",
+                "--no-warnings",
+                "-f",
+                "18",
+                &format!("https://youtu.be/{id}"),
+            ])
+            .output()
+            .unwrap()
+            .stdout,
+    )
+    .unwrap()
+    .trim_end()
+    .to_string()
 }
